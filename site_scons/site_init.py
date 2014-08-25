f7c6b5920a5f06ca067e104d86622a0d7f55234e
William D. Jones <thor0505@comcast.net>
2014-08-25 11:00:24 -0400

    Forgot to commit site_init.py

---
+++ site_scons/site_init.py
@@ -0,0 +1,70 @@
+def perform_compiler_configuration(env, freestanding):
+	if not env.GetOption('clean') and not env.GetOption('help'):
+		conf = Configure(env, config_h='cc_config.h', custom_tests=\
+			{ 'CheckCharBit' : check_char_bit })
+		conf.CheckHeader('stdio.h', include_quotes='<>', language='C')
+		conf.CheckHeader('time.h', include_quotes='<>', language='C')
+		conf.CheckType('double')
+		#conf.CheckFunc('difftime', '#include <time.h>'):
+		conf.CheckDeclaration('difftime', '#include <time.h>')
+		conf.CheckCharBit()	
+			
+		conf.Finish()
+		
+		
+#def check_little_endian(env):
+#	context.Message('Checking whether target is little endian... ' )
+#	lastLIBS = context.env['LIBS']
+#	lastLIBPATH = context.env['LIBPATH']
+#	lastCPPPATH= context.env['CPPPATH']
+#	context.env.Append(LIBS = 'qt', LIBPATH = qtdir + '/lib', CPPPATH = qtdir + '/include' )
+#	ret = context.TryCompile("""
+#		#include <qapp.h>
+#		int main(int argc, char **argv) {
+#		QApplication qapp(argc, argv);
+#		return 0;
+#		}
+#		""")
+#if not ret:
+#	context.env.Replace(LIBS = lastLIBS, LIBPATH=lastLIBPATH, CPPPATH=lastCPPPATH)
+#context.Result( ret )
+#return ret
+
+def check_char_bit(context):
+	context.Message('Checking whether target has an 8-bit character... ' )
+	ret = context.TryCompile("""
+		#include <limits.h>
+		#if CHAR_BIT != 8
+			#error
+		#endif
+		int main(int argc, char **argv)
+		{
+		return 0;
+		}""", '.c')
+	
+	SCons.Conftest._Have(context, 'HAVE_CHAR_BIT_8', ret, 'Set to 1 if the target has an 8-bit character.')
+	context.Result(ret)
+	return ret
+		
+
+platform_signatures = {'DOS': ['__DOS__'], 'WIN32': ['__WINDOWS__', '_WIN32']}
+
+def platform_sanity_checks(env):
+	if not env.GetOption('clean') and not env.GetOption('help'):
+		conf = Configure(env, config_h='cc_config.h') #, custom_tests = \
+			#{ 'CheckLibCProvidesBDB' : CheckLibCProvidesBDB })
+		if not conf.CheckCC():
+			print 'Error: C compiler does not work!'
+			Exit(1)
+		
+		for sig in platform_signatures[env['TARGET_OS']]:
+			if conf.CheckDeclaration(sig):
+				break
+		else:
+			print 'Error: The chosen C compiler {0} doesn''t appear ' + \
+				'to support the chosen target {1}! ' + \
+				+ 'Check platform flags!'.format(env['TOOLS'], \
+				env['TARGET_OS'])
+			Exit(1)
+			
+		conf.Finish()
