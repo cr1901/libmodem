f3ead3d767f458d6be1d56d5b9467c8ab1bff75b
William D. Jones <thor0505@comcast.net>
2014-12-22 00:00:12 -0500

    Clean up serprim.h, finish creating the serial routine portion of the test suite (all 41 assertions pass).

--- test/serprim.c
+++ test/serprim.c
@@ -1,47 +1,145 @@
 /* Dummy serial device class. Has access to extern vars to control the output
 destination, whether the device is working properly, etc. */
 
+#include "shared.h"
+
 #include "serial.h"
 #include "serprim.h"
 #include <stddef.h> /* For NULL. */
 
+/* The buffers used to test the modem routines. May be split from file_bufs
+eventually. */
+/* extern char local_tx_file_buf[STATIC_BUFSIZ];
+extern char local_rx_file_buf[STATIC_BUFSIZ];
+extern char remote_tx_file_buf[STATIC_BUFSIZ];
+extern char remote_rx_file_buf[STATIC_BUFSIZ]; */
+
+
+/* Simulate device buffers at some point- some UARTs (glares at 8250) don't have one! */
+/* extern char local_tx_device_buf[STATIC_BUFSIZ];
+extern char local_rx_device_buf[STATIC_BUFSIZ];
+extern char remote_tx_device_buf[STATIC_BUFSIZ];
+extern char remote_rx_device_buf[STATIC_BUFSIZ]; */
+
+/* Line is modelled by a an array- abstract representation of characters being 
+sent down the wire before being acknowledged by receiever. When appropriate, perhaps
+model individual tx/rx using multitasking? *shrug* */
+char local_tx_line[STATIC_BUFSIZ];
+char local_rx_line[STATIC_BUFSIZ];
+char * remote_tx_line = local_rx_line; /* Connect the virtual serial lines to each other. */
+char * remote_rx_line = local_tx_line;
+
+PORT_DESC port_model[3] = { {NULL, NULL, 0, 0, 0, 0, 0} };
+
+
+/* Needed as dummy target of void ptr at some point? */
+/* static int local_handle = 0;
+static int remote_handle = 1; */
+
 serial_handle_t open_handle(unsigned short port_no)
-{
-	return (void *) 0xCAFE;
+{	
+	return port_model + port_no;
 }
 
 int handle_valid(serial_handle_t port)
 {
-	return 1;
+	return ((port == port_model) || (port == port_model + 1) || (port == port_model + 2));
 }
 
+/* We can assume a valid handle here. However, on error, the handle will
+be globally set to invalid. */
 int init_port(serial_handle_t port, unsigned long baud_rate)
 {
+	(void) baud_rate;
+	
+	if(port == &port_model[0])
+	{
+		VOID_TO_PORT(port, tx_line) = local_tx_line;
+		VOID_TO_PORT(port, rx_line) = local_rx_line;
+	}
+	else if(port == &port_model[1])
+	{
+		VOID_TO_PORT(port, tx_line) = remote_tx_line;
+		VOID_TO_PORT(port, rx_line) = remote_rx_line;
+	}
+	else
+	{
+		return -1;
+	}
+	
+	VOID_TO_PORT(port, bad_write) = 0;
+	VOID_TO_PORT(port, bad_read) = 0;
+	VOID_TO_PORT(port, force_rx_timeout) = 0;
+	
 	return 0;
 }
 
 int valid_size(unsigned int num_bytes)
-{
-	return 1;
+{	
+	return (num_bytes <= STATIC_BUFSIZ);
 }
 
 int write_data(serial_handle_t port, char * data, unsigned int num_bytes)
 {
-	return 0;
+	if(!VOID_TO_PORT(port, bad_write))
+	{
+		unsigned int count;
+		for(count = 0; count < num_bytes; count++)
+		{
+			((PORT_DESC *) port)->tx_line[count] = data[count];
+		}
+		
+		return 0;
+	}
+	else
+	{
+		return -1;
+	}
 }
 
 int read_data(serial_handle_t port, char * data, unsigned int num_bytes, int timeout)
 {
-	return 0;
+	(void) timeout;
+	
+	if(VOID_TO_PORT(port, bad_read))
+	{
+		return -2;
+	}
+	else if(VOID_TO_PORT(port, force_rx_timeout))
+	{
+		return -1;
+	}
+	else
+	{
+		unsigned int count;
+		for(count = 0; count < num_bytes; count++)
+		{
+			 data[count] = ((PORT_DESC *) port)->rx_line[count];
+		}
+		
+		return 0;
+	}
 }
 
 int close_handle(serial_handle_t port)
 {
-	return 0;
+	return VOID_TO_PORT(port, bad_close);
 }
 
 int flush_device(serial_handle_t port)
-{
+{	
+	if(!VOID_TO_PORT(port, bad_flush))
+	{
+		unsigned int count;
+		for(count = 0; count < STATIC_BUFSIZ; count++)
+		{
+			((PORT_DESC *) port)->rx_line[count] = '\0';
+		}
+	}
+	else
+	{
+		return -1;
+	}
 	return 0;
 }
 
--- test/unittest.c
+++ test/unittest.c
@@ -1,73 +1,242 @@
 #include "minunit.h"
+#include "shared.h"
 
-static int foo = 0;
-static int bar = 0;
-static double dbar = 0.1;
+#include "serial.h"
+#include "modem.h"
 
+#include <limits.h>
+
+typedef struct verify_params
+{
+	int force_resend;
+	
+}VERIFY_PARAMS;
+
+
+
+/* For future full duplex consideration, have tx/rx bufs for both. */
+char local_tx_file_buf[STATIC_BUFSIZ] = {'\0'};
+char local_rx_file_buf[STATIC_BUFSIZ] = {'\0'};
+char remote_tx_file_buf[STATIC_BUFSIZ] = {'\0'};
+char remote_rx_file_buf[STATIC_BUFSIZ] = {'\0'};
+
+serial_handle_t local_port, remote_port;
+VERIFY_PARAMS ver_params;
+
+/* Data xfer fcns used as XMODEM callbacks. */
+/* int verify_data_out_fcn(char * buf, const int request_size, const int last_sent_size, void * const chan_state); */
+/* int verify_data_in_fcn(char * buf, const int request_size, const int last_sent_size, void * const chan_state); */
+
+/* Helper functions. */
+static void fill_buf(char * buf, unsigned int num_chars);
+static int buf_cmp(char * buf1, char * buf2);
+static void buf_clr(char * buf, unsigned int num_chars);
+
+
+/* Test setup clears all buffers and assumes a working serial port. */
 void test_setup() {
-	foo = 7;
-	bar = 4;
+	serial_init(LOCAL, 115200, &local_port);
+	serial_init(REMOTE, 115200, &remote_port);
+	buf_clr(local_tx_file_buf, STATIC_BUFSIZ);
+	buf_clr(local_rx_file_buf, STATIC_BUFSIZ);
+	buf_clr(remote_tx_file_buf, STATIC_BUFSIZ);
+	buf_clr(remote_rx_file_buf, STATIC_BUFSIZ);
 }
 
 void test_teardown() {
-	/*  Nothing */
+	serial_close(&local_port);
+	serial_close(&remote_port);
 }
 
-MU_TEST(test_check) {
-	mu_check(foo == 7);
+MU_TEST(test_ser_bad_handle)
+{
+	mu_check(serial_close(&local_port) == SERIAL_NO_ERRORS);
+	fill_buf(local_tx_file_buf, 128);
+	mu_check(serial_init(BAD_DEVICE, 115200, &local_port) == SERIAL_HW_ERROR);
+	/* mu_check(local_port == &port_model[BAD_DEVICE]); Make no assumptions about port value on init error? 
+	Or set to NULL/guaranteed invalid handle? The former implies "no guarantees about what happens 
+	when calling other functions with a bad handle." Any need to have access to/otherwise
+	manipulate handles directly? */
+	mu_check(serial_snd(local_tx_file_buf, 128, local_port) == SERIAL_HW_ERROR);
+	mu_check(serial_close(&local_port) == SERIAL_HW_ERROR);
+	
+	mu_check(serial_close(&remote_port) == SERIAL_NO_ERRORS);
+	mu_check(serial_init(BAD_HANDLE, 115200, &remote_port) == SERIAL_HW_ERROR);
+	mu_check(remote_port == NULL);
+	mu_check(serial_rcv(remote_rx_file_buf, 128, 1, remote_port) == SERIAL_HW_ERROR);
+	mu_check(serial_flush(remote_port) == SERIAL_HW_ERROR);	
+	mu_check(serial_close(&remote_port) == SERIAL_HW_ERROR);
+	
+	mu_check(buf_cmp(local_tx_file_buf, remote_rx_file_buf) == 0);
 }
 
-MU_TEST(test_check_fail) {
-	mu_check(foo != 7);
+MU_TEST(test_ser_tx)
+{
+	fill_buf(local_tx_file_buf, 128);	
+	mu_check(serial_snd(local_tx_file_buf, 128, local_port) == SERIAL_NO_ERRORS);
+	mu_check(serial_rcv(remote_rx_file_buf, 128, 1, remote_port) == SERIAL_NO_ERRORS);
+	mu_check(buf_cmp(local_tx_file_buf, remote_rx_file_buf) == 1);
 }
 
-MU_TEST(test_assert) {
-	mu_assert(foo == 7, "foo should be 7");
+MU_TEST(test_ser_rx_hw_error_bad_read)
+{
+	VOID_TO_PORT(remote_port, bad_read) = 1;
+	fill_buf(local_tx_file_buf, 128);
+	mu_check(serial_snd(local_tx_file_buf, 128, local_port) == SERIAL_NO_ERRORS);
+	mu_check(serial_rcv(remote_rx_file_buf, 128, 1, remote_port) == SERIAL_HW_ERROR);
+	mu_check(buf_cmp(local_tx_file_buf, remote_rx_file_buf) == 0);	
 }
 
-MU_TEST(test_assert_fail) {
-	mu_assert(foo != 7, "foo should be <> 7");
+MU_TEST(test_ser_tx_hw_error_bad_write)
+{
+	VOID_TO_PORT(local_port, bad_write) = 1;
+	fill_buf(local_tx_file_buf, 128);
+	/* Nothing was sent in this example, but the point is there was an error
+	when trying to fulfill the request. */
+	mu_check(serial_snd(local_tx_file_buf, 128, local_port) == SERIAL_HW_ERROR);
+	
+	mu_check(buf_cmp(local_tx_file_buf, remote_rx_file_buf) == 0); 
+	mu_check(serial_rcv(remote_rx_file_buf, 128, 1, remote_port) == SERIAL_NO_ERRORS);
+	mu_check(buf_cmp(local_tx_file_buf, remote_rx_file_buf) == 0);	
 }
 
-MU_TEST(test_assert_int_eq) {
-	mu_assert_int_eq(4, bar);
+MU_TEST(test_ser_rx_timeout)
+{
+	VOID_TO_PORT(remote_port, force_rx_timeout) = 1;
+	fill_buf(local_tx_file_buf, 128);
+	mu_check(serial_snd(local_tx_file_buf, 128, local_port) == SERIAL_NO_ERRORS);
+	mu_check(serial_rcv(remote_rx_file_buf, 128, 1, remote_port) == SERIAL_TIMEOUT);
+	mu_check(buf_cmp(local_tx_file_buf, remote_rx_file_buf) == 0);	
 }
 
-MU_TEST(test_assert_int_eq_fail) {
-	mu_assert_int_eq(5, bar);
+MU_TEST(test_ser_rx_flush_close)
+{
+	fill_buf(local_tx_file_buf, 128);	
+	mu_check(serial_snd(local_tx_file_buf, 128, local_port) == SERIAL_NO_ERRORS);
+	/* Assume: at this point, the data has been sent, but has not been 
+	retrieved from the remote's internal rcv buffer. */
+	mu_check(serial_flush(remote_port) == SERIAL_NO_ERRORS);
+	mu_check(serial_rcv(remote_rx_file_buf, 128, 1, remote_port) == SERIAL_NO_ERRORS);
+	/* Because of the flush, the buffers should not be equal... serial_rcv
+	should have received a fresh buffer full of '\0' :). */
+	mu_check(buf_cmp(local_tx_file_buf, remote_rx_file_buf) == 0);
+	
+	/* Simulate a bad flush. */
+	VOID_TO_PORT(remote_port, bad_flush) = 1;
+	mu_check(serial_snd(local_tx_file_buf, 128, local_port) == SERIAL_NO_ERRORS);
+	mu_check(serial_flush(remote_port) == SERIAL_HW_ERROR);
+	mu_check(serial_rcv(remote_rx_file_buf, 128, 1, remote_port) == SERIAL_NO_ERRORS);
+	
+	/* Because the flush failed, the buffers should be equal now (in this
+	test suite, flush is all or nothing). */
+	mu_check(buf_cmp(local_tx_file_buf, remote_rx_file_buf) == 1);
+	mu_check(serial_close(&remote_port) == SERIAL_HW_ERROR);
+	VOID_TO_PORT(remote_port, bad_flush) = 0;
 }
 
-MU_TEST(test_assert_double_eq) {
-	mu_assert_double_eq(0.1, dbar);
+MU_TEST(test_ser_close)
+{
+	fill_buf(local_tx_file_buf, 128);	
+	mu_check(serial_snd(local_tx_file_buf, 128, local_port) == SERIAL_NO_ERRORS);
+	
+	/* Simulate bad close due to bad flush. (If closing flush failed, this
+	test suite assumes the serial port is still open for use). */
+	VOID_TO_PORT(remote_port, bad_flush) = 1;
+	mu_check(serial_close(&remote_port) == SERIAL_HW_ERROR);
+	mu_check(serial_rcv(remote_rx_file_buf, 128, 1, remote_port) == SERIAL_NO_ERRORS);
+	mu_check(buf_cmp(local_tx_file_buf, remote_rx_file_buf) == 1);
+	
+	VOID_TO_PORT(local_port, bad_close) = 1;
+	mu_check(serial_close(&local_port) == SERIAL_HW_ERROR);
+	
+	VOID_TO_PORT(remote_port, bad_close) = 1;
+	mu_check(serial_close(&remote_port) == SERIAL_HW_ERROR);   
+	                                                          
+	VOID_TO_PORT(remote_port, bad_flush) = 0;
+	VOID_TO_PORT(local_port, bad_close) = 0;
+	VOID_TO_PORT(remote_port, bad_close) = 0;
+	
+	mu_check(serial_close(&local_port) == SERIAL_NO_ERRORS);
+	mu_check(serial_close(&remote_port) == SERIAL_NO_ERRORS);   
 }
 
-MU_TEST(test_assert_double_eq_fail) {
-	mu_assert_double_eq(0.2, dbar);
-}
+/* MU_TEST(test_ser_edge)
+{
+	Reserved. For ex: valid_size test, perhaps in the future.	
+} */
 
-MU_TEST(test_fail) {
-	mu_fail("Fail now!");
-}
+/* MU_TEST(test_xmodem_packet)
+{
+	fill_buf(local_tx_file_buf, 128);
+} */
 
-
-MU_TEST_SUITE(test_suite) {
+MU_TEST_SUITE(ser_test_suite) 
+{
 	MU_SUITE_CONFIGURE(&test_setup, &test_teardown);
-
-	MU_RUN_TEST(test_check);
-	MU_RUN_TEST(test_assert);
-	MU_RUN_TEST(test_assert_int_eq);
-	MU_RUN_TEST(test_assert_double_eq);
-
-	MU_RUN_TEST(test_check_fail);
-	MU_RUN_TEST(test_assert_fail);
-	MU_RUN_TEST(test_assert_int_eq_fail);
-	MU_RUN_TEST(test_assert_double_eq_fail);
-
-	MU_RUN_TEST(test_fail);
+	
+	MU_RUN_TEST(test_ser_tx);
+	MU_RUN_TEST(test_ser_tx_hw_error_bad_write);
+	
+	MU_RUN_TEST(test_ser_rx_hw_error_bad_read);
+	MU_RUN_TEST(test_ser_rx_timeout);
+	MU_RUN_TEST(test_ser_rx_flush_close);
+	
+	MU_RUN_TEST(test_ser_bad_handle);
+	MU_RUN_TEST(test_ser_close);
+	/* MU_RUN_TEST(test_ser_edge); */
+	
+	
+	/* MU_RUN_TEST(xmodem_test); */
 }
 
-int main(int argc, char *argv[]) {
-	MU_RUN_SUITE(test_suite);
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	(void) argv;
+	
+	MU_RUN_SUITE(ser_test_suite);
 	MU_REPORT();
 	return 0;
+}
+
+
+/* int verify_data_out_fcn(char * buf, const int request_size, const int last_sent_size, void * const chan_state)
+{
+	
+	
+} */
+
+
+static void fill_buf(char * buf, unsigned int num_chars)
+{
+	unsigned int cur;
+	  
+	for(cur = 0; cur < num_chars; cur++)
+	{
+		buf[cur] = cur;
+	}
+}
+
+static int buf_cmp(char * buf1, char * buf2)
+{
+	int cur;
+	int tx_okay = 1;
+	
+	for(cur = 0; (tx_okay && cur < 128); cur++)
+	{
+		tx_okay = (((unsigned char *) buf1)[cur] == ((unsigned char *) buf2)[cur]);
+	}
+	
+	return tx_okay;
+}
+
+static void buf_clr(char * buf, unsigned int num_chars)
+{
+	unsigned int cur;
+	  
+	for(cur = 0; cur < num_chars; cur++)
+	{
+		buf[cur] = '\0';
+	}
 }
