cc456e5a2412f73feba60ae797b311acbbb330a8
William D. Jones <thor0505@comcast.net>
2017-08-16 03:23:12 -0400

    Add serprim implementation for HDMI2USB.

--- src/hdmi2usb-lm32/serprim.c
+++ src/hdmi2usb-lm32/serprim.c
@@ -1,44 +1,107 @@
 #include "serial.h"
 #include "serprim.h"
 
+#include <time.h>
+#include <uart.h>
 #include <stddef.h>
+#include <generated/csr.h>
+#include <hw/common.h>
+
 
 serial_handle_t open_handle(unsigned short port_no)
 {
-    return NULL;
+    return (port_no == 0) ? (serial_handle_t) CSR_UART_BASE : NULL;
 }
 
 int handle_valid(serial_handle_t port)
 {
-    return 0;
+    return (MMPTR(CSR_UART_BASE) == MMPTR(port));
 }
 
 int init_port(serial_handle_t port, unsigned long baud_rate)
 {
+    /* BIOS will initialize uart and timer. */
+    (void) port;
+    (void) baud_rate;
     return 0;
 }
 
 int write_data(serial_handle_t port, char * data, unsigned int num_bytes)
 {
+    (void) port;
+    unsigned int count;
+
+    for(count = 0; count < num_bytes; count++)
+    {
+        if(!uart_txfull_read())
+        {
+            uart_write(data[count]);
+        }
+        else
+        {
+            /* Assume unrecoverable if we fill tx buffer. */
+            return -2;
+        }
+    }
+
     return 0;
 }
 
 int read_data(serial_handle_t port, char * data, unsigned int num_bytes, int timeout)
 {
-    return 0;
+    unsigned int count = 0;
+    int start_time = 0;
+
+    (void) port;
+
+    elapsed(&start_time, -1); /* Initialize elapsed function. */
+    while(!elapsed(&start_time, timeout * SYSTEM_CLOCK_FREQUENCY))
+    {
+        if(uart_read_nonblock())
+        {
+            data[count++] = uart_read();
+            if(count >= num_bytes)
+            {
+                return 0;
+            }
+        }
+    }
+
+    return -1;
 }
 
 int read_data_get_elapsed_time(serial_handle_t port, char * data, unsigned int num_bytes, int timeout, int * elapsed)
 {
-    return 0;
+    int start_time, end_time;
+    int rv;
+
+    start_time = timer0_value_read();
+    rv = read_data(port, data, num_bytes, timeout);
+    end_time = timer0_value_read();
+
+    if(start_time < end_time)
+    {
+        /* Down counter. Timer rolled over if start < end. */
+        end_time += timer0_reload_read();
+    }
+
+    *elapsed = (end_time - start_time) / SYSTEM_CLOCK_FREQUENCY;
+    return rv;
 }
 
 int close_handle(serial_handle_t port)
 {
+    (void) port;
     return 0;
 }
 
 int flush_device(serial_handle_t port)
 {
+    (void) port;
+    while(uart_read_nonblock())
+    {
+        (void) uart_read();
+    }
+
     return 0;
 }
