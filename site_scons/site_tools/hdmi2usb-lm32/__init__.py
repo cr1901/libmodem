e3603ead3525d70a316c7d6add50542a42a54d6d
William D. Jones <thor0505@comcast.net>
2017-08-18 13:54:21 -0400

    HDMI2USB tool support was never committed.

---
+++ site_scons/site_tools/hdmi2usb-lm32/__init__.py
@@ -0,0 +1,2 @@
+from hdmi2usb_lm32 import generate
+from hdmi2usb_lm32 import exists
---
+++ site_scons/site_tools/hdmi2usb-lm32/hdmi2usb_lm32.py
@@ -0,0 +1,28 @@
+import os.path
+import SCons.Tool.gcc
+import SCons.Util
+
+def import_vars(env):
+    build_settings = os.path.join(env['HDMI2USB_BUILD'], "software", "include", "generated", "variables.mak")
+
+    with open(build_settings, 'r') as fp:
+        hdmi_dict = dict()
+        for var in fp.read().split('\n'):
+            try:
+                (name, val) = var.split("=")
+                # When using gcc build, SCons doesn't treat "C:/" as a
+                # top-level. Manually replace.
+                hdmi_dict[name] = val.replace("\\\\", "/").replace("\r", "").replace("C:/", "/c/")
+            except:
+                # Skip "export" line
+                pass
+        env['HDMI2USB'] = hdmi_dict
+
+def exists(env):
+    return SCons.Tool.gcc.exists(env)
+
+def generate(env):
+    import_vars(env)
+    env["CC"] = "lm32-elf-gcc" # Setting CC directly allows override of
+    # default gcc.
+    SCons.Tool.gcc.generate(env)
